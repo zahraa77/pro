a83fb37d3b31d5252e165a129bd0a95b1519759a
alzahraa553@gmail.com <you@example.com>
2019-07-24 13:20:49 +0300

    ADD

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$ (function() {
+  'use strict';
+ var winH =$(window).height(),
+     upperH =$('.upper-bar').innerHeight(),
+     navH  =$ ('.navbar').innerHeight();
+
+
+     $('.slider .carousel-item').height(winH -(upperH+navH));
+
+
+});
